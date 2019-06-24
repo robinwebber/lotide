c7feaba0a12cf30b2250c922e55d2c8c528190b3
robinwebber <robinewebber@gmail.com>
2019-06-24 20:51:34 +0000

    Tail of array function for lotide (Lighthouse)

---
+++ tail.js
@@ -0,0 +1,25 @@
+// Tail function for lotide activity (Lighthouse)
+
+// Copy and paste of assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i <= array.length - 1; i++) {
+    newArray.push(array[i]);
+  } return newArray;
+};
+
+console.log(tail([1, 2, 3, 4, 5]));
+console.log(tail(['hey', 'how', 'are', 'you']));
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
