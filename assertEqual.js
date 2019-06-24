0431d5a9c05f8bd566ec2a3d36f12babc29cdc0d
robinwebber <robinewebber@gmail.com>
2019-06-24 19:55:23 +0000

    Assert Equal Exercise for Lighthouse

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('❌ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+
+// TEST CODE
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('good', 1);
+assertEqual('hey', 'hey');
+assertEqual(3, 7);
