a7442fa5531f70f3d6dc4f132885009a46a53bac
robinwebber <robinewebber@gmail.com>
2019-06-24 20:27:59 +0000

    head function for lotide (Lighthouse)

---
+++ head.js
@@ -0,0 +1,25 @@
+// Head function exercise for lotide (Lighthouse)
+
+// Copy and pasted assertEqual function from previous exercise
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  let first = array[0];
+  return first;
+};
+
+// test to make sure I am getting my expected output
+// console.log(head([3, 10, 15, 20]));
+// console.log(head(['hey', 'how\'s', 'it', 'going?']));
+// console.log(head(['this is the only thing here']));
+// console.log(head([]));
+
+// test using assertEqual function
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
