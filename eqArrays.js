73b86ae9072564f35d4c57adc4019e3091e3f993
robinwebber <robinewebber@gmail.com>
2019-06-25 22:52:47 +0000

    not sure what I changed, best refer to previous version

--- eqArrays.js
+++ eqArrays.js
@@ -9,16 +9,17 @@ const assertEqual = function(actual, expected) {
 // create two empty arrays to hold the entered info
 const eqArrays = function (firstArray, secondArray) {
   // first compare the lengths of the two arrays if not equal return false
+  let result = true;
   if (firstArray.length !== secondArray.length) {
-    return false;
+    result = false;
   } else {
     // compare elements of the arrays with a loop
     for (let i = 0; i < firstArray.length; i++) {
       // if array1[i] === array2[i] then continue
       if (firstArray[i] !== secondArray[i]) {
-        return false;
+        result = false;
       }
-    } return true;
+    } return result;
 
   }
 }
