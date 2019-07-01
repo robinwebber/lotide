9e119c1428788e5e761f19a195516f14d3aa8a25
robinwebber <robinewebber@gmail.com>
2019-07-01 20:14:04 +0000

    updated middleTest to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+// Pair coded with Nelly Main
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+const middle = require('../middle');
+const assert = require('chai').assert;
+
+describe('#middleTest', () => {
+  it('should return an empty array for an array with 1 element', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('should return an empty array for an array with 2 elements', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it('for an array with an odd number of elements it should return the middle element', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it('for an array with an even number of elements it should return an array with the 2 middle elements', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
+
